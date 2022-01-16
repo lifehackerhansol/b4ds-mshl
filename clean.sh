1be86b99e782b93ab265ed2db8331041b2187012
lifehackerhansol <lifehacker@hansol.ca>
2022-01-15 21:40:04 -0800

    First working version

---
+++ clean.sh
@@ -0,0 +1,6 @@
+make clean
+make -C bootloader clean
+make -C bootstub clean
+make -C nds-exception-stub clean
+make -C libands clean
+rm -rf data
--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2021 lifehackerhansol
+Copyright (c) 2021-present lifehackerhansol
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,9 +20,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include <stdio.h>
+#include <cstdio>
 #include <string>
 #include <vector>
+#include <unistd.h>
 
 #include <nds.h>
 #include <fat.h>
@@ -30,8 +31,11 @@ SOFTWARE.
 #include "extlink.h"
 #include "nds_loader_arm9.h"
 
-int fail(void){
-	iprintf("Boot fail.\nPress START to power off.");
+int fail(std::string debug){
+	consoleDemoInit();
+	iprintf("extlink2argv boot fail:\n");
+	iprintf(debug.c_str());
+	iprintf("\nPress START to power off.");
 	while(1) {
 		swiWaitForVBlank();
 		scanKeys();
@@ -42,40 +46,39 @@ int fail(void){
 }
 
 int main(void) {
-	consoleDemoInit();
-	fatInitDefault();
-	iprintf("Reading extlink...\n");
+	if(!fatInitDefault()) {
+		return fail("FAT init fail.");
+	}
 	FILE* f = fopen("/moonshl2/extlink.dat","r+b");
-	char* target;
 	TExtLinkBody extlink;
 	if (f) {
 		memset(&extlink, 0, sizeof(TExtLinkBody));
 		fread(&extlink, 1, sizeof(TExtLinkBody),f);
 		if(extlink.ID != ExtLinkBody_ID) {
 			fclose(f);
-			return fail();
+			return fail("Extlink ID mismatch.");
 		}
-		ucs2tombs((unsigned char*)target, extlink.DataFullPathFilenameUnicode, 768);
 		fseek(f,0,SEEK_SET);
 		fwrite("____", 1, 4, f);
 		fflush(f);
 		fclose(f);
+	} else return fail("Extlink does not exist.");
+	char target[768];
+	ucs2tombs((unsigned char*)target, extlink.NDSFullPathFilenameUnicode, 768);
+	std::string txtpath;
+	for(u32 i = 0; i < strlen(target); i++){
+		txtpath.push_back(target[i]);
 	}
-	iprintf("Reading ini...\n");
-	iprintf(extlink.NDSFullPathFilenameAlias);
-	iprintf("\n");
-	std::string inipath;
-	for(u32 i = 0; i < strlen(extlink.NDSFullPathFilenameAlias); i++){
-		inipath.push_back(extlink.NDSFullPathFilenameAlias[i]);
-	} 
-	iprintf(inipath.c_str());
-	inipath = inipath.erase(inipath.size() - 4) + ".txt";
-	f = fopen(inipath.c_str(), "r");
+	txtpath = txtpath.erase(txtpath.size() - 4) + ".txt";
+	f = fopen(txtpath.c_str(), "r");
+	if (!f) return fail("Path text file does not exist.");
 	std::vector<char*> argarray;
 	argarray.push_back(strdup("NULL"));
-	if(fgets(argarray[0], 768, f) == NULL) return fail();
-	argarray.at(1) = extlink.DataFullPathFilenameAlias;
-	iprintf("Launching extlink...\n");
+	argarray.push_back(strdup("NULL"));
+	ucs2tombs((unsigned char*)target, extlink.DataFullPathFilenameUnicode, 768);
+	fgets(argarray[0], 768, f);
+	argarray.at(1) = target;
+	if(access(argarray[0], F_OK) != 0) return fail("Bootstrap target does not exist.");
 	int err = runNdsFile (argarray[0], argarray.size(), (const char **)&argarray[0]);
-	if (err != 0) return fail();
+	if (err != 0) return fail("NDS Launch error " + err);
 }
